505f3831130bf118fd537fa52a6a4ed4f1280136
roku36 <roku36000@gmail.com>
2023-08-09 21:07:06 +0900

    car movement

--- Loop/Scripts/player.gd
+++ Loop/Scripts/player.gd
@@ -1,28 +1,26 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const turn_spd : float = 0.03
+const SPEED = 30.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var direction : float = 0
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	var input_vector = Vector2.ZERO
+	var handle_input = Input.get_axis("ui_left", "ui_right")
+	
+	direction += turn_spd * handle_input
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	self.rotation = direction
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	# move toward direction vector
+	if Input.is_action_pressed("ui_accept"):
+		input_vector = Vector2.UP.rotated(direction)
+		# velocity = velocity.move_toward(input_vector, SPEED)
+		velocity += input_vector * SPEED 
+
+	# damp velocity
+	velocity *= 0.95
 
 	move_and_slide()
