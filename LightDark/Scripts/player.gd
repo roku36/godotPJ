a30a4296fe6a69423ccf30f39cdb9030dc38abe1
roku36 <roku36000@gmail.com>
2023-08-14 22:10:13 +0900

    move character, tell light/dark with modulation

--- LightDark/Scripts/player.gd
+++ LightDark/Scripts/player.gd
@@ -1,18 +1,33 @@
+# @tool
 extends Node2D
 
 @onready var subViewport = $"../SubViewportContainer/SubViewport"
 
 var image: Image
-
-func _ready() -> void:
-	var texture :ViewportTexture = subViewport.get_texture()
-	print(texture)
-	image = texture.get_image()
-	print(image)
+var spd: float = 0.5
 
 func _process(delta: float) -> void:
-	# var color = image.get_pixelv(self.position)
-	# print("color!!")
-	# var color = image.get_pixelv(Vector2(100, 100))
-	# print(color)
-	print(image)
+	# move left/right/up/down by keyboard input
+	if Input.is_action_pressed("ui_left"):
+		self.position.x -= spd
+	if Input.is_action_pressed("ui_right"):
+		self.position.x += spd
+	if Input.is_action_pressed("ui_up"):
+		self.position.y -= spd
+	if Input.is_action_pressed("ui_down"):
+		self.position.y += spd
+
+
+	var texture :ViewportTexture = subViewport.get_texture()
+	image = texture.get_image()
+	var color = image.get_pixelv(self.position)
+	
+	# reset modulation
+	self.modulate = Color(1, 1, 1)
+	
+	if color.r > 0.7:
+		self.modulate = Color(1, 0, 0)
+	
+	if color.r < 0.4:
+		self.modulate = Color(0, 1, 0)
+
