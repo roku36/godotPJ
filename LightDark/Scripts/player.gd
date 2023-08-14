86f5def87604826597158c8f0ba4d4e135f24993
roku36 <roku36000@gmail.com>
2023-08-15 00:13:07 +0900

    jittering...

--- LightDark/Scripts/player.gd
+++ LightDark/Scripts/player.gd
@@ -1,22 +1,23 @@
 # @tool
-extends Node2D
+extends CharacterBody2D
 
-@onready var subViewport = $"../SubViewportContainer/SubViewport"
-
+# @onready var subViewport = $"../SubViewportContainer/SubViewport"
+@onready var subViewport = $".."
 var image: Image
-var spd: float = 0.5
+var spd: float = 50
 
 func _process(delta: float) -> void:
-	# move left/right/up/down by keyboard input
-	if Input.is_action_pressed("ui_left"):
-		self.position.x -= spd
-	if Input.is_action_pressed("ui_right"):
-		self.position.x += spd
-	if Input.is_action_pressed("ui_up"):
-		self.position.y -= spd
-	if Input.is_action_pressed("ui_down"):
-		self.position.y += spd
+	var mouse_pos = get_global_mouse_position()
 
+	# move toward cursor position on screen in constant spd with normalization if left click
+	# use move_and_slide
+	velocity = Vector2.ZERO
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		var direction = (mouse_pos - self.position).normalized()
+		velocity = direction * spd
+		
+
+	
 
 	var texture :ViewportTexture = subViewport.get_texture()
 	image = texture.get_image()
@@ -24,10 +25,13 @@ func _process(delta: float) -> void:
 	
 	# reset modulation
 	self.modulate = Color(1, 1, 1)
+	print(color)
 	
 	if color.r > 0.7:
 		self.modulate = Color(1, 0, 0)
 	
-	if color.r < 0.4:
+	if color.r < 0.3:
 		self.modulate = Color(0, 1, 0)
+
+	move_and_slide()
 
