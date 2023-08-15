7cf5337e65b463459c7bcad6d752245c73519655
roku36 <roku36000@gmail.com>
2023-08-16 06:40:41 +0900

    complete light instance

--- LightDark/Scripts/light.gd
+++ LightDark/Scripts/light.gd
@@ -1,5 +1,21 @@
 @tool
 extends Node2D
 
-@export var direction : Vector2 = Vector2.UP
-@export var width : float
+@onready var point_light_2d: PointLight2D = $PointLight2D
+
+@export var lightScale : float = 1.0
+@export var enerty : int = 10
+@export var height : float = 20.0
+
+func _ready() -> void:
+	update_params()
+
+# set export variables to child pointlight2d
+func update_params() -> void:
+	point_light_2d.scale = Vector2(lightScale, lightScale)
+	point_light_2d.energy = enerty
+	point_light_2d.height = height
+
+# run update_params() when exported values change
+func _process(_delta: float) -> void:
+	update_params()
