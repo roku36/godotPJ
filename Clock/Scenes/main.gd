6d549f1d1a971edaa27be791eae2ca44978a6132
roku36 <roku36000@gmail.com>
2023-09-01 20:34:01 +0900

    time tick

--- Clock/Scenes/main.gd
+++ Clock/Scenes/main.gd
@@ -1,12 +1,20 @@
 extends Node2D
-
+@onready var color_rect: ColorRect = $ColorRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# set window passthrough to true
-	Window.mouse_passthrough = true
-
+	# DisplayServer.window_set_mouse_passthrough([])
+	DisplayServer.window_set_mouse_passthrough( [Vector2(0,0), Vector2(100,0), Vector2(100,100), Vector2(0,100)] )
+	# get_viewport().transparent_bg = true
+	get_tree().get_root().set_transparent_background(true)
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_TRANSPARENT, true, 0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	# set color rect shader parameters to current time (Hours, Minutes, Seconds)
+	var timeDict = Time.get_time_dict_from_system()
+	# print(timeDict["second"])
+	color_rect.material.set_shader_parameter("hours", timeDict["hour"])
+	color_rect.material.set_shader_parameter("minutes", timeDict["minute"])
+	color_rect.material.set_shader_parameter("seconds", timeDict["second"])
